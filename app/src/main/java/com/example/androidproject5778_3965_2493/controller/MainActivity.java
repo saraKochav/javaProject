e7fa3ff531240f2b82aa9683b1031ef02ef647a2
chaya bar <chaya.bar770@gmail.com>
2018-01-03 11:46:56 +0200

    16 tevet

--- app/src/main/java/com/example/androidproject5778_3965_2493/controller/MainActivity.java
+++ app/src/main/java/com/example/androidproject5778_3965_2493/controller/MainActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 
 import com.example.androidproject5778_3965_2493.R;
 
-public class MainActivity extends Activity {
+public class MainActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
